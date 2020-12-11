12e4ce81d5a0f4dda746caaad520dc2ce8e5cc5f
Osman Başar Akkaya <akkayabasar@gmail.com>
2020-12-12 00:30:10 +0300

    Created fetch product api

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const axios = require('axios');
+const config = require('config');
 
-router.get('/', async (req, res) => {
-  res.send('hello');
-});
+/**
+ * @route       GET api/products
+ * @description Get products containing the keyword within their title.
+ *              Request body should contain attribute `keyword`
+ *              in order not to throw validation error.
+ * @access      Public
+ */
+router.get(
+  '/',
+  [check('keyword', 'Please enter a keyword').not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      console.error('Validation error');
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const products = await axios.get(
+        `https://${config.get('apiKey')}:${config.get(
+          'apiSecret'
+        )}@${config.get('apiURL')}/admin/products.json?title=${
+          req.body.keyword
+        }&fields=id,title`
+      );
+
+      return res.status(200).json(products.data);
+    } catch (error) {
+      console.error('An Error Occurred: ', error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
